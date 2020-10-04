f1add0944f424a639fc1c2e9f2098269d66ee950
Tatiana Zilio <tatianazilio@gmail.com>
2020-10-04 17:38:28 -0300

    initial commit

---
+++ src/main/java/com/example/springbootexample/datasource/postgresDatasource.java
@@ -0,0 +1,21 @@
+package com.example.springbootexample.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.internal.database.h2.H2Connection;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class postgresDatasource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
